26e17fc78496e751c00f2e9b6459c291e7d1aa25
Michel <106924948+michelsodre@users.noreply.github.com>
2024-11-21 12:35:03 -0300

    feat : Delete User

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -2,13 +2,15 @@ import React, { useContext, useEffect, useState } from 'react'
 import './styles.css'
 import { GlobalContext } from '../../context'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 function User() {
     //Context
     const context = useContext(GlobalContext)
-    const { loggedUser, listCandidaturas, setListCandidaturas } = context
+    const { setLoggedUser, setIsLogged, loggedUser, setIsOng, listCandidaturas, setListCandidaturas } = context
     //States
     const [listWorkDetails, setListWorkDetails] = useState([])
+    const navigate = useNavigate()
     //Functions
     function dataToString(params) {
         const date = new Date(Date.parse(params)).toLocaleDateString()
@@ -35,6 +37,23 @@ function User() {
         const data = await response.json()
         setListCandidaturas(data.myAdditions)
     }
+    ////Deletar Conta do usuário
+    async function DeleteUser() {
+        try {
+            //deletear todas as inscrições em vagas
+            await axios.delete(`http://localhost:5000/api/addition/deletemany/${loggedUser._id}`)
+            await axios.delete(`http://localhost:5000/api/voluntary/delete/${loggedUser._id}`)
+
+        } catch (error) {
+            console.log(error);
+
+        }
+        await setLoggedUser({ birth_date: "", email: "", name: "", phone: "", _id: "" })
+        await setIsLogged(false)
+        await setIsOng(false)
+        navigate('/')
+    }
+
     ////Buscar detalhes de cada trabalho após carregar a lista de candidaturas
     useEffect(() => {
         if (listCandidaturas.length > 0) {
@@ -43,6 +62,13 @@ function User() {
             });
         }
     }, [listCandidaturas])
+    ////condicional caso tentativa de acesso sem login
+    useEffect(() => {
+        if (!loggedUser._id) {
+            navigate('/');
+        }
+    }, [loggedUser]);
+
 
     return (
         <>
@@ -54,6 +80,7 @@ function User() {
                 <p>Data de nascimento: {dataToString(loggedUser.birth_date)}</p>
                 <p>E-mail: {loggedUser.email}</p>
                 <p>Telefone: {loggedUser.phone}</p>
+                <button onClick={() => DeleteUser()}>Deletar Conta</button>
                 <h1>Candidaturas</h1>
                 {listCandidaturas && listCandidaturas.length
                     ? (listCandidaturas.map((item, index) =>
@@ -66,7 +93,7 @@ function User() {
                     </div>)))
                     : (<p>Sem candidaturas</p>)
                 }
-            </div>
+            </div >
         </>
 
     )
